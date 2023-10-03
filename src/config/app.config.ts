9be887d4283006cc8299d5dedfdb73bee21b13b0
Franz Villarroel C. <villarroel.cf@gmail.com>
2023-10-03 11:24:08 -0400

    modificacion de configuracion

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,4 +1,4 @@
-export const AppConfiguration =()=>({
+export const AppConfiguration  = () => ({
     environment:process.env.NODE_ENV || 'dev',
     mongodb: process.env.MONGODB,
     port:process.env.PORT || 3002,
